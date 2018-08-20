40aa543945336b2e1ad046a0f971f1a9137f1cd5
Steven Shirley <stevenshirley@Stevens-MBP.router>
2018-08-20 02:15:22 -0500

    added all files

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+// AOS.init();
+
+
+
+// let doc = document.documentElement;
+// let element = document.querySelector('#aboutmeparani')
+// document.addEventListener('scroll', function(){
+//  if(doc.scrollTop + document.height === element.getBoundingClientRect().top){
+//     element.style.opacity = 1;
+//     element.style.offset = '50%'
+// }
+// });
+//
+// console.log(element);
